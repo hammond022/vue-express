25c2f6d2b11c0da271e1f9be87ba60efa2a3d7bb
hammond022 <75580640+hammond022@users.noreply.github.com>
2025-08-27 11:56:33 +0800

    first commit

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+const mongodb_key = process.env.MONGODB_KEY;
+
+// Get Posts
+router.get("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  await res.send(await posts.find({}).toArray());
+});
+
+// Add Post
+
+router.post("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+  res.status(201).send();
+});
+
+//Delete Post
+
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
+  res.status(200).send();
+});
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect(
+    `mongodb+srv://dontpeelyourapple:${mongodb_key}@cluster0.w93qyhx.mongodb.net/`
+  );
+  return client.db("vue_express").collection("posts");
+}
+module.exports = router;
